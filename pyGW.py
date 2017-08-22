9c0af2d53d890adacd3fc204a5ef717c2e03cb09
David Culbreth <dculbreth@mail.stmarytx.edu>
2017-08-21 22:10:34 -0500

    things with turns, restructure Actor out of main file.

--- pyGW.py
+++ pyGW.py
@@ -1,99 +1,13 @@
 import tkinter
-true = True # for simplicity...
-none = None
+from Actor import *
 
-directionMap = {"north": 0, "northeast": 45, "east": 90, "southeast": 135, "south": 180, "southwest": 225, "west": 270, "northwest": 315}
-
-class Actor :
-    def __init__(self, img = None, grid = None, loc = None, color = "blue", dir = 0):
-        if grid != None or grid.__class__ == "GridWorld":
-             self.__grid = grid
-        if loc == None or loc.__class__ != "<class 'tuple'>"or len(loc)!=2:
-            self.__loc = (0,0)
-        else:
-            self.__loc = loc
-
-
-    def getColor (self):
-        return self.color
-    def getDirection(self):
-        return
-    def setDirection(self, direction):
-        if isinstance(direction, int):
-            self.direction = direction%360
-        elif isinstance(direction, str):
-            direction = direction.lower()
-            if directionMap.has_key(direcion):
-                self.direction = directionMap[direction]
-            else:
-                self.direction = 0
-        return
-    def setLocation(self):
-        return
-    def move(self):
-        """Moves forward: in the rounded direction that the Actor is facing.
-              0 (N)
-          7(NW) ^ 1(NE)
-      6(W)    <-+->     2(E)
-          5(SW) V 3(SE)
-              4(S)
-        The Actor's direction will be approximated to make the movement comply
-        to one of the 8 surrounding location possibilities.
-        this function does NOT check for out of bounds, and (should?) throw an error for out of bounds.
-        """
-        movementVector = (0,0)
-        moveDirection = int(round((self.direction%360)/45.0))%8
-        if moveDirection == 0: # north
-            movementVector = (-1,0 )# row, col
-        elif moveDirection == 1: # northeast
-            movementVector = (-1,1) # row, col
-        elif moveDirection == 2: # east
-            movementVector = (0,1)
-        elif moveDirection == 3: # southeast
-            movementVector = (1,1)
-        elif moveDirection == 4: # south
-            movementVector = (1,0)
-        elif moveDirection == 5: #southwest
-            movementVector = (1,-1)
-        elif moveDirection == 6: # west
-            movementVector = (0,-1)
-        elif moveDirection == 7: # northwest
-            movementVector = (-1,-1)
-
-        self.loc = (self.loc[0] + movementVector[0], self.loc[1] + movementVector[1])
-        #remove self from old loc
-
-        #add self to new grid loc.
-
-        return
-    def setColor (self, color):
-        self.color = color
-        self.ui_el
-
-    def act(self):
-        self.direction = (self.direction + 180)%360
-        return
-    def removeSelfFromGrid(self):
-        node = grid.getNode(self.loc)
-        node.actor = None
-        return True
-    def addSelfToGrid(self, grid, loc):
-        node = grid.getNode(loc)
-        if node.actor != None:
-            node.actor.removeSelfFromGrid()
-        node.actor = self
-        grid.actors.append(self)
-        self.grid = grid
-        self.loc = loc
-        node.updateUI()
-        return True
 
 class Node:
     def clickDefault(self):
         # color = "#" + str()
         # self.ui_el.config(bg = "red", text = str(self.loc[0]) + ", " + str(self.loc[1]))
         a = Actor()
-        a.addSelfToGrid(self.grid, self.loc)
+        a.addSelfToGrid(grid = self.grid, loc = self.loc)
         return
     def __init__(self, grid, ui_el = None, loc = None, actor = None, clickFunction = None):
         self.grid = grid
@@ -106,9 +20,10 @@ class Node:
             if self.grid.elementType == "button":
                 self.ui_el.config(command = self.clickDefault) #don't bother with frames, the pain isn't worth it.
 
-        if loc != None  and  str(loc.__class__) == "<class 'tuple'>"  and  len(loc) == 2:
+        if loc != None  and  loc.__class__ == tuple and  len(loc) == 2:
             self.loc = loc
         else:
+            print(str(loc.__class__) + " : : " + str(tuple))
             raise ValueError ("A Node must be initialized with a location. Please assign an location to loc.\n"+
                 "loc: " + str(loc.__class__) + ": length " + str(len(loc)))
         self.actor = actor
@@ -122,12 +37,13 @@ class GridWorld:
         self.playing = not self.playing
         if self.playing == False:
             print("\nPaused!")
-        else: print("\nPlaying!")
-        self.turn()
+        else:
+            print("\nPlaying!")
+            self.turn()
         return
     def stepButtonPress(self):
         self.playing = False
-        self.turn()
+        self.turn(override = True)
         return
     def __init__(self, width = None, height = None, scale = None, elementType = "button", turnDelay = 200, resizeable = False, autoRun = False):
         if width != None:
@@ -201,13 +117,14 @@ class GridWorld:
         self.app.geometry(str(self.width * self.scale)+"x"+str((self.height + 1) * self.scale))
         self.app.resizable(width = resizeable, height = resizeable)
 
-
-
-    def getNode(self, row = 0, col = 0, loc = None):
-        if loc != None and loc.__class__ == (0,0).__class__ and len(loc) >= 2:
+    def getNode(self, loc = None):
+        if loc.__class__ == tuple and len(loc) == 2:
             row = loc[0]
             col = loc[1]
-        return nodes[row, col]
+            return self.nodes[row, col]
+        else:
+            raise ValueError("The function GridWorld.getNode(loc) requires a tuple of length 2 as an argument.")
+            return None
 
     def isOccupied(self, loc):
         if self.getNode(loc = loc).actor != None:
@@ -220,14 +137,15 @@ class GridWorld:
             self.app.after(self.turnDelay, self.turn)
         self.app.mainloop()
 
-    def turn(self):
-        print("\n Turn " + str(self.turnCount))
-        for a in self.actors:
-            a.act()
-        if self.playing == True:
-            self.app.after(self.turnDelay, self.turn)
-        self.turnLabel.config(text = "Turn: " + str(self.turnCount))
-        self.turnCount += 1
+    def turn(self, override = False):
+        if override or self.playing:
+            print("\n Turn " + str(self.turnCount))
+            for a in self.actors:
+                a.act()
+            if self.playing == True:
+                self.app.after(self.turnDelay, self.turn)
+            self.turnLabel.config(text = "Turn: " + str(self.turnCount))
+            self.turnCount += 1
 
 
 def test():
